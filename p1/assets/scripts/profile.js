859d9e28cab56da7eac5c7b72d71fb655355a5e6
akshay-nair <akshay.nair@mail.utoronto.ca>
2016-07-13 23:24:57 -0400

    Moved some images. Added post page. Some edits.

--- p1/assets/scripts/profile.js
+++ p1/assets/scripts/profile.js
@@ -3,34 +3,19 @@ var posts = [
         "author": "John Doe",
         "title": "Looking to get your smartphone repaired?!",
         "type": "Offering",
+		"rating": "4/5",
         "picture": "",
         "contact_info": "111-111-1111",
-        "description": "If you're looking to get your smartphone repaired, look no further! I have been repairing smartphone for the past 5 years and I can guarantee that your smartphone will be fixed in less than 5 business days!",
-        "comments": [
-            {
-                "author": "A A",
-                "comment": "How much do you charge to replace the screen on an iPhone 6?"
-            },
-            {
-                "author": "James A",
-                "comment": "Can you fix a water damaged phone?"
-            }
-        ]
+        "description": "If you're looking to get your smartphone repaired, look no further! I have been repairing smartphone for the past 5 years and I can guarantee that your smartphone will be fixed in less than 5 business days!"
     },
     {
         "author": "Jane D",
         "title": "Looking for someone to fix an iPhone 4",
         "type": "Searching",
-		"urgency": "4/5",
+		"rating": "4/5",
         "picture": "",
         "contact_info": "416-000-1234",
-        "description": "hi, im looking for someone who can fix my iPhone 4 and replace the screen for me. pls msg me if you can help!",
-        "comments": [
-            {
-                "author": "John Doe",
-                "comment": "Hi, I am a smartphone technician and I can easily replace the screen on your iPhone. Please feel free to message me for a quote on the repair :)"
-            }
-        ]
+        "description": "hi, im looking for someone who can fix my iPhone 4 and replace the screen for me. pls msg me if you can help!"
     }
 ];
 
@@ -43,21 +28,15 @@ function initialize() {
         $(curr_id).append($('<p/>', {class: 'post_type', id: 'type-' + i}));
         if (posts[i].type === 'Searching') {
             $('#type-' + i).text("Searching for service");
-			$(curr_id).append($('<p/>', {class: 'post_type', text: "Urgency: " + posts[i].urgency}));
+			$(curr_id).append($('<p/>', {class: 'post_type', text: "Urgency: " + posts[i].rating}));
         } else {
             $('#type-' + i).text("Offering service");
+			$(curr_id).append($('<p/>', {class: 'post_type', text: "Reputation: " + posts[i].rating}));
         }
         if (posts[i].contact_info != '') {
             $(curr_id).append($('<p/>', {class: 'contact-info', text: 'Contact Info: ' + posts[i].contact_info}));
         }
         $(curr_id).append($('<p/>', {class: 'description', text: "Description: " + posts[i].description}));
-        $(curr_id).append($('<section/>', {class: 'comments', id: "post-" + i + "-comments"}));
-        $("#post-" + i + "-comments").append($('<p/>', {text: "Comments: "}));
-        for (j = 0; j < posts[i].comments.length; j ++) {
-            $("#post-" + i + "-comments").append($('<hr/>'));
-            $("#post-" + i + "-comments").append($('<p/>', {class: 'comment', id: 'comment-' + j + '-author', text: posts[i].comments[j].author + ": "}));
-            $("#post-" + i + "-comments").append($('<p/>', {class: 'comment', id: 'comment-' + j + '-comment', text: posts[i].comments[j].comment}));
-        }
     }
 }
 
