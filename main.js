037ceb21275e92190636301130acfabe7c928207
akshay-nair <akshay.nair@mail.utoronto.ca>
2016-07-22 15:01:27 -0400

    Hossein: Started back-end and populated addresses. Akshay: Finished SQL with Hossein's changes.

---
+++ main.js
@@ -0,0 +1,152 @@
+var express = require('express');
+var promise = require('bluebird'); 
+var emailer = require('nodemailer'); 
+var requestIp = require('request-ip'); 
+var bodyParser = require('body-parser');
+var pg = require('pg');
+//var app = express();
+
+var trans = emailer.createTransport('smtps://reputech.team%40gmail.com:222aaaAAA@smtp.gmail.com');
+
+var options = {
+    promiseLib: promise 
+};
+
+var cn = {
+    host: 'localhost', 
+    port: 3000,
+    database: 'reputech',
+    user: 'postgres',
+    password: '551360'
+};
+
+var pgc = require('pg-promise')(options);
+var db = pgc(cn);
+var app = new express();
+
+var connectpath = 'postgres://postgres:551360@localhost:3000/reputech';
+
+app.get('/get_countries',function(req,res){
+       console.log("countries");
+       db.any("select * from countries order by  name ASC")
+	   .then(function(data){
+	      res.writeHead(200,{'Content-Type':'text/plain','Access-Control-Allow-Origin':'*'});
+          res.end(JSON.stringify(data));
+		  pgc.end(); 
+	   })
+	   .catch(function(error){
+	      console.log("ERROR====>>>",error);
+       })
+})
+
+app.get('/get_provinces',function(req,res){
+
+       db.any("select state from countries,states WHERE countries.code = states.country AND countries.name = $1 ORDER BY state ASC ",[req.query.country])
+	  .then(function(data){
+	      res.writeHead(200,{'Content-Type':'text/plain','Access-Control-Allow-Origin':'*'});
+          res.end(JSON.stringify(data));
+		  pgc.end(); 
+	  })
+	  .catch(function(error){
+	      console.log("ERROR====>>>",error);
+      })
+})
+
+app.get('/store_info',function(req,res){
+       console.log("recieved to store");
+       var email = req.query.email;
+	   
+	   db.any("SELECT u_id FROM LOGIN WHERE email=$1",[email])
+	   .then(function(data){
+	         if(data.length > 0 ) {
+			    res.writeHead(200,{'Content-Type':'text/plain','Access-Control-Allow-Origin':'*'});
+                res.end(JSON.stringify({"status":"exists"}));
+		        pgc.end(); 
+			 }else{
+			    storeInfo(req,res);
+			    var textBody = "This is your verification email from Reputech. In order to verify your email "+
+				                "address and activate your account please click on the link below"; 
+				
+				var htmlContent	= "<a href='http://www.google.com'> Please Click On This Link To Activate Your Account.</a>";
+				
+				sendEmailto(email,"Verification Email For Your Reputech Account",textBody,htmlContent)
+				
+                res.writeHead(200,{'Content-Type':'text/plain','Access-Control-Allow-Origin':'*'});
+                res.end(JSON.stringify({"status":"ok"}));
+		   	 }		 
+			
+	   })
+	   .catch(function(error){
+	      console.log("ERROR====>>>",error);
+       })
+   
+})
+		  
+app.listen(8080,function(){
+      console.log("server is listening.....");
+})	 
+
+
+function storeInfo(req,res){
+
+       var clientIp = req.ip;
+	   var email = req.query.email;
+	   var pass = req.query.pass;
+	   var name = req.query.name;
+	   var lastname = req.query.lastname;
+	   var country = req.query.country;
+	   var state = req.query.state;
+	   var city = req.query.city;
+	   var phone = req.query.phone;
+	   var addrId;
+	   
+	   pg.connect(connectpath, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({ success: false, data: err});
+        }
+		
+		client.query("INSERT INTO LOGIN(email,user_type,ip_address) VALUES($1,$2,$3) returning u_id",[email,'Normal',clientIp]);
+		
+		var sql = "SELECT u_id FROM LOGIN WHERE email= '"+email+"'";
+		var sqladdr = "SELECT a_id FROM ADDRESS WHERE ( ( country = '"+country+"' AND province = '"+state+"') AND city = '"+city+"' )";
+		
+		client.query(sql,function(err,info){
+		
+             if(err) 		
+		         console.log("ERROR =========>>>> \n"+err);
+				 
+		      	 var clientId = JSON.stringify(info.rows[0].u_id);
+				 client.query("INSERT INTO AUTH(u_id,password,privilege) VALUES($1,$2,$3)",[clientId,pass,"Normal"]);
+			     client.query("INSERT INTO ADDRESS(country,province,city) VALUES($1,$2,$3)",[country,state,city]);
+				 
+		         client.query(sqladdr,function(err,info){
+				 
+	                      if(err) 		
+		                         console.log("ERROR =========>>>> \n"+err);
+								 
+						  var addrId = JSON.stringify(info.rows[0].a_id);		 
+						  client.query("INSERT INTO USERS VALUES($1,$2,$3,$4,$5)",[clientId,name,lastname,phone,addrId]);
+				});
+	    });
+		
+	 });
+}
+
+function sendEmailto(reciever,subjContent,textBody,htmlContent){
+  var mail = {
+       from: 'hossein.freedom@gmail.com', // sender address 
+       to: reciever, // list of receivers 
+       subject: subjContent, // Subject line 
+       text: textBody,// plaintext body      
+       html: htmlContent // html body 
+  };
+  trans.sendMail(mail,function(error,info){
+	 if(error){
+	    return  console.log(error);
+	 } 
+	 console.log('Message sent: ' + info.response);	   
+  });
+}
---
+++ public/assets/scripts/signup.js
@@ -0,0 +1,134 @@
+function initialize() {
+    	
+	$(".error").hide();
+	populateCountries();
+	$("#submit").on("click", validateEvent);
+}
+
+function validateEvent(e) {
+	var valid = true;
+	$("input").each(function(index, elem) {
+	     alert($(elem).attr("id"));
+		if($(elem).attr("id") != "search"){ 
+		  if ( !validate($(elem)) ){
+			valid = false;
+	      }
+	    }
+	});
+		
+	if (!valid) {
+		e.preventDefault();
+	}
+	if (valid) {
+   	  submitForm();
+	}  
+}
+
+function validate($elem) {
+	var $err = $("#" + $elem.attr("id") + "-err");
+	if ($elem.val() == "") { // 'Required' message will auto handle
+		setBorderColor($elem, "red");
+		$err.text("This field is required.").fadeIn();
+		return false;
+	}
+	setBorderColor($elem, "#ccc");
+	$err.hide();
+	
+	var r = new RegExp($elem.attr("pattern"));
+	if (!r.test($elem.val())) {
+		var prompt = null;
+		if ($elem.attr("type") == 'password') {
+			prompt = "Password must be between 8-15 characters, and must consist of at least one lower-case letter, one upper-case letter and one digit.";
+		} else if ($elem.attr("type") == 'text') {
+			prompt = "This field can only consist of letters.";
+		} else if ($elem.attr("type") == 'email') {
+			prompt = "E-mail address must be valid.";
+		}
+		if (prompt != null) {
+			$err.text(prompt).fadeIn();
+		}
+		setBorderColor($elem, "red");
+		return false;
+	}
+	if (($elem.attr("id") == "email" || $elem.attr("id") == "pass")
+			&& ($elem.val() != $("#" + $elem.attr("id") + "2").val())) {
+				
+		$err.text("Confirmation fields must be the same.").fadeIn();
+		setBorderColor($elem, "red");
+		return false;
+	}
+	
+	return true;
+}
+
+function setBorderColor($elem, color){
+	$elem.css("border", "1px solid " + color);
+}
+
+function populateCountries(){
+     $.ajax({
+	     type:"GET",
+	     dataType: "json",
+		 url:"http://127.0.0.1:8080/get_countries",
+		 success: function(res){
+		             $.each(res,function(index,item){
+					      var option = $('<option>');
+						  option.attr('value',item.name);
+						  option.html(item.name);
+					      $('#country').append(option);
+					 });					 
+		          },
+		  error: function(){
+			 	     alert("error!!");								                              	   	          
+                 }}); 
+}
+
+function populateStates(){
+    $.ajax({
+	     type:"GET",
+	     dataType: "json",
+		 url:"http://127.0.0.1:8080/get_provinces?country="+$('#country').val(),
+		 success: function(res){
+		             $('#state').empty();
+					 
+                     if( res.length == 0 ) 					 
+                          $('#state').append('<option selected disabled>State/Province</option>');					 
+						  
+		             $.each(res,function(index,item){
+					      var option = $('<option>');
+						  option.attr('value',item.state);
+						  option.html(item.state);
+					      $('#state').append(option);
+					 });					
+		          },
+		  error: function(){
+			 	     alert("error!!");								                              	   	          
+                 }}); 
+}
+
+
+function submitForm(){
+    $("#busy_sec").css("visibility","visible"); 
+    $.ajax({
+	     type:"GET",
+	     dataType: "json",
+		 url:"http://127.0.0.1:8080/store_info?email="+$('#email').val()+"&pass="+$('#pass').val()+"&name="+$('#name').val()+
+		                              "&lastname="+$('#lastname').val()+"&country="+$('#country').val()+"&state="+$('#state').val()
+									  +"&city="+$('#city').val()+"&phone="+$('#phone').val(),
+		 success: function(res){
+		             $("#busy_sec").css("visibility","hidden"); 
+		             if (res.status === "exists"){
+					    setBorderColor($("#email"), "red");
+			            $("#email-err").text("This Email Address is Already Used. Please Use Another Email Address.").fadeIn();
+					 }else{
+					    $("main").empty();    
+						$("main").append("<h3>Verification Email Has Been Send to The Email Address You Used to Sign up. Please Verify Your Email As Soon As possible in Order to Activate Your Account.</h3>");
+						
+					 }
+		          },
+		 error: function(){
+		       alert("error");
+         }}); 
+}
+
+$(document).ready(initialize);
